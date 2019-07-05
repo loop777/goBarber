40308a4295c053aa7a85f9c238f27f7f4e2c0bea
Ricardo Alcantara <ricardo_alcantara2@hotmail.com>
2019-07-04 21:02:25 -0300

    Envio de emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '7c4098d82077a6',
+    pass: '30e4aabd6fc541',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
